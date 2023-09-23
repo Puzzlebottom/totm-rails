ea89e88b981eaaa632a0ac3fab3913777390e34e
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-22 21:16:35 -0400

    adds has_secure_password to user model

--- totm-backend/app/models/user.rb
+++ totm-backend/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :encounters
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(username, password)
+    user = User.find_by_username(username)
+    return unless user&.authenticate(password)
+
+    user
+  end
 end
