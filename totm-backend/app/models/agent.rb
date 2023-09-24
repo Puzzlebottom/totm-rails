e70dd05ff3f07154185b5278ed512ab90966c80a
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-24 12:11:03 -0400

    fixes typo

--- totm-backend/app/models/agent.rb
+++ totm-backend/app/models/agent.rb
@@ -1,10 +1,8 @@
 class Agent < ApplicationRecord
   belongs_to :encounter, optional: true
-  has_many :clusters, :through => :agents_clusters
+  has_many :clusters, through: :agents_clusters
 
   validates :name, presence: true
   validates :initiative, presence: true
   validates :encounter_id, presence: true
-
-  def self.add_cluster(cluster_id)
 end
