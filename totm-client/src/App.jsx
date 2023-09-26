957dedc70aecad091d740f884ae6109c2690f4ae
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-25 20:11:22 -0400

    updates imports

--- totm-client/src/App.jsx
+++ totm-client/src/App.jsx
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import { useState } from 'react';
 import NavBar from './components/NavBar';
 import Authorization from './components/Authorization';
 
--- totm-client/src/components/Authorization.jsx
+++ totm-client/src/components/Authorization.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 const Authorization = () => {
   const initialState = { username: '', password: '', password_confirmation: '' };
   const [state, setState] = useState(initialState);
