7909feb23bb00fd287d57e5d95371c21472fa1a8
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-09-25 20:10:45 -0400

    moves user form to Authorization element

--- totm-client/src/App.jsx
+++ totm-client/src/App.jsx
@@ -1,34 +1,13 @@
 import axios from 'axios';
 import { useState } from 'react';
 import NavBar from './components/NavBar';
+import Authorization from './components/Authorization';
 
 function App() {
-  const initialState = { username: '', password: '', password_confirmation: '' };
-  const [state, setState] = useState(initialState);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('http://localhost:3000/users', { user: state })
-      .then(res => {
-        console.log(res);
-        setState(initialState);
-      })
-      .catch(err => console.log(err));
-  };
-
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
-
   return (
     <>
       <NavBar />
-      <form onSubmit={handleSubmit}>
-        <input type='text' name={'username'} placeholder={'Enter Username'} value={state.username} onChange={handleChange} />
-        <input type='password' name={'password'} placeholder={'Enter Password'} value={state.password} onChange={handleChange} />
-        <input type='password' name={'password_confirmation'} placeholder={'Confirm Password'} value={state.password_confirmation} onChange={handleChange} />
-        <button type='submit'>SUBMIT</button>
-      </form>
+      <Authorization />
     </>
   );
 }
---
+++ totm-client/src/components/Authorization.jsx
@@ -0,0 +1,28 @@
+const Authorization = () => {
+  const initialState = { username: '', password: '', password_confirmation: '' };
+  const [state, setState] = useState(initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/users', { user: state })
+      .then(res => {
+        console.log(res);
+        setState(initialState);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' name={'username'} placeholder={'Enter Username'} value={state.username} onChange={handleChange} />
+      <input type='password' name={'password'} placeholder={'Enter Password'} value={state.password} onChange={handleChange} />
+      <input type='password' name={'password_confirmation'} placeholder={'Confirm Password'} value={state.password_confirmation} onChange={handleChange} />
+      <button type='submit'>SUBMIT</button>
+    </form>
+  );
+};
+
+export default Authorization;
